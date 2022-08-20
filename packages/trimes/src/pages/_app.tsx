543573e40a0f885f2c9352484ed4631c5ff97bae
jkugsiya <jkugsiya11@gmail.com>
2022-08-21 02:23:31 +0530

    index page changes

--- packages/trimes/src/pages/_app.tsx
+++ packages/trimes/src/pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'swiper/css'
+import { Header } from '../components/Header'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <div className="pt-20">
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
 
 export default MyApp
--- packages/trimes/src/pages/index.tsx
+++ packages/trimes/src/pages/index.tsx
@@ -1,13 +1,75 @@
+import { Editorial } from '@prisma/client'
 import type { NextPage } from 'next'
-import { Header } from '../components/Header'
+import Image from 'next/image'
+import { Autoplay, Pagination } from 'swiper'
+import 'swiper/css/pagination'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { prismaClient } from '../lib/prisma'
 
-const Home: NextPage = () => {
+export const getServerSideProps = async () => {
+  const editorial = await prismaClient.editorial.findFirst({
+    orderBy: { createdAt: 'desc' }
+  })
+
+  console.log(editorial)
+  if (!editorial)
+    return {
+      props: {}
+    }
+  return {
+    props: {
+      editorial: {
+        title: editorial?.title,
+        body: editorial?.body,
+        createdAt: new Date(editorial?.createdAt).toLocaleString()
+      }
+    }
+  }
+}
+
+const Home: NextPage<{ editorial: Editorial }> = ({ editorial }) => {
   return (
     <>
-      <Header ></Header>
-      <div className=" text-white ">
-        Tailwind is working if background is red!
-      </div>
+      {editorial && (
+        <div className="px-48 flex space-x-12">
+          <div>
+            <h2 className="text-3xl font-semibold italic">मन की बात</h2>
+            <span>{editorial.createdAt.toLocaleString()}</span>
+          </div>
+          <div>
+            <h3 className="text-xl font-semibold">{editorial.title}</h3>
+            <p
+              className="mt-2"
+              dangerouslySetInnerHTML={{
+                __html: editorial.body.replaceAll('\n', '<br/>')
+              }}
+            ></p>
+          </div>
+        </div>
+      )}
+
+      <Swiper
+        className="mt-20"
+        modules={[Pagination, Autoplay]}
+        pagination={{
+          dynamicBullets: true
+        }}
+        autoplay={{
+          delay: 2000
+        }}
+      >
+        {[...Array(10)].map((_, index) => (
+          <SwiperSlide key={index}>
+            <Image
+              src={`/trimes-photos/${index + 1}.jpeg`}
+              alt="trimes-photo"
+              layout="responsive"
+              width={1280}
+              height={720}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </>
   )
 }
