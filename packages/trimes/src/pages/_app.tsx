5e06fabca47212eae319379e9006c38bbbc640a7
jkugsiya <jkugsiya11@gmail.com>
2022-08-21 05:28:35 +0530

    merge solved

--- packages/trimes/src/pages/_app.tsx
+++ packages/trimes/src/pages/_app.tsx
@@ -1,32 +1,25 @@
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import 'swiper/css'
+import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const queryClient = new QueryClient()
   return (
-<<<<<<< HEAD
     <SessionProvider session={session}>
       <QueryClientProvider client={queryClient}>
         <Header />
         <div className="pt-20">
           <Component {...pageProps} />
         </div>
+        <Script src="/chatra.js" />
+        <Footer />
       </QueryClientProvider>
     </SessionProvider>
-=======
-    <>
-      <Header />
-      <div className="pt-20">
-        <Component {...pageProps} />
-      </div>
-      <Script src="/chatra.js" />
-      <Footer />
-    </>
->>>>>>> 0ddb84ca768bd9ee1795794af2f92ce5eea28cad
   )
 }
 
