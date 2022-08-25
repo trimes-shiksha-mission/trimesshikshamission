9b85726985deb39213dbf691f54fae90df575ab3
jkugsiya <jkugsiya11@gmail.com>
2022-08-25 21:51:29 +0530

    changes

--- packages/trimes/src/pages/api/auth/[...nextauth].ts
+++ packages/trimes/src/pages/api/auth/[...nextauth].ts
@@ -4,17 +4,17 @@ import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { prismaClient } from '../../../lib/prisma'
 
+declare module 'next-auth' {
+  interface Session {
+    user?: User
+  }
+}
+
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
       name: 'credentials',
 
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
         contact: {
           label: 'contact',
@@ -59,7 +59,7 @@ export default NextAuth({
     },
     async session({ session, token }) {
       if (token.user) {
-        session.user = token.user as any
+        session.user = token.user as User
       }
       return session
     }
