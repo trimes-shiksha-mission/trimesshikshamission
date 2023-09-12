fc63a3c0295f5261b95fa8a3781353cf1ffbb9f3
jkugsiya <jkugsiya11@gmail.com>
2023-09-13 00:45:57 +0530

    fixed pages with auth

--- site/src/pages/tsm/tsm-educational-institute.tsx
+++ site/src/pages/tsm/tsm-educational-institute.tsx
@@ -1,6 +1,19 @@
-import { NextPage } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
 import { Blogs } from '~/components/Blogs'
 import { Layout } from '~/components/Layout'
+import { getServerAuthSession } from '~/server/auth'
+
+export const getServerSideProps: GetServerSideProps = async ctx => {
+  const session = await getServerAuthSession(ctx)
+  return {
+    redirect: !session
+      ? {
+          destination: '/login'
+        }
+      : undefined,
+    props: {}
+  }
+}
 
 const TSMEducationalInstitue: NextPage = () => {
   return (
